ed00b7149b9b3360e23bafcbcc3ac51c44a80dbc
Shpiler7 <pcts79@gmail.com>
2018-04-16 00:15:29 +0200

    Added few more tests

--- myTest.py
+++ myTest.py
@@ -1,5 +1,6 @@
 import unittest
 from flight_simulator import Plane
+from flight_simulator import Simulator
 import math
 #https://github.com/karsta26/kol1_gr2.git
 
@@ -7,6 +8,7 @@ class MyTest(unittest.TestCase):
 
 	def setUp(self):
 		self.test_instance = Plane()
+		self.test_instance_simulator = Simulator(self.test_instance)
 
 	def test_start_orientation(self):
 		self.assertEqual(self.test_instance.orientation, 0)
@@ -19,3 +21,29 @@ class MyTest(unittest.TestCase):
 
 	def test_crash_start(self):
 		self.assertEqual(self.test_instance.check_crash(), None)
+
+	def test_if_plane_exists(self):
+		self.assertNotEqual(self.test_instance, None)
+
+	def test_if_simulator_exists(self):
+		self.assertNotEqual(self.test_instance, None)
+
+	def test_correction_if_greater_than_zero(self):
+		new_orientation = 5
+		self.test_instance.orientation = new_orientation
+		self.test_instance_simulator.apply_tilt_correction()
+		self.assertEqual(self.test_instance.orientation, new_orientation-20)
+
+	def test_correction_if_lesser_than_zero(self):
+		new_orientation = -5
+		self.test_instance.orientation = new_orientation
+		self.test_instance_simulator.apply_tilt_correction()
+		self.assertEqual(self.test_instance.orientation, new_orientation+20)
+	
+	def test_correction_if_equal_zero(self):
+		self.test_instance_simulator.apply_tilt_correction()
+		self.assertEqual(self.test_instance.orientation, 0)
+
+	def test_turbulations(self):
+		self.test_instance_simulator.turbulations()
+		self.assertNotEqual(self.test_instance.orientation, 0)
